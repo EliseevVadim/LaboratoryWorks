4f16536fb9829e46d07f533b7c35ca7381eb6f25
EliseevVadim <eliseevv02@mail.ru>
2019-10-21 22:45:31 +0300

    7.1

---
+++ 7.1/7.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if ((a>=b)&&(b>=c))
+            {
+                
+                Console.WriteLine(a-c); // ac
+            }
+           else if ((b>=a)&&(a>=c))
+            {
+                
+                Console.WriteLine(b-c); //bc
+            }
+            else if ((a>=c)&&(c>=b)) //ab
+            {
+                
+                Console.WriteLine(a-b);
+            }
+            else if ((b>=c)&&(c>=a))
+            {
+                
+                Console.WriteLine(b-a); //ba
+            }
+            else if ((c>=b)&&(b>=a))
+            {
+                
+                Console.WriteLine(c-a); //ca
+            }
+            else if ((c>=a)&&(a>=b))
+            {
+                
+                Console.WriteLine(c-b); //cb
+            }
+            
+            Console.ReadKey();
+            
+
+        }
+    }
+}
