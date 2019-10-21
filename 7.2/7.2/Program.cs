da6031b8ba005938f56b386613224f882f97a2ee
EliseevVadim <eliseevv02@mail.ru>
2019-10-21 22:44:21 +0300

    7.2

---
+++ 7.2/7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=10; i<100; i++)
+            {
+                int a = i / 10;
+                int b = i % 10;
+                int c = (a + b) % 5;
+                if (c==0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
