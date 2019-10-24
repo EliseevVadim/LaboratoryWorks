afe4084bea0a3c6c7fc4ce01f7e9cbff5fb627bc
EliseevVadim <eliseevv02@mail.ru>
2019-10-24 19:59:56 +0300

    Task2 (Clean Version)

---
+++ 1488/Task2(Clean Version)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_Clean_Version_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int N = int.Parse(Console.ReadLine());
+            double total = 1;
+            double b = 1;
+            for (int i=3;i<=N; i+=2)
+            {
+                if (counter%2==0)
+                {
+                    total -= b / i;
+                }
+                else
+                {
+                    total += b / i;
+                }
+                counter++;
+            }
+            Console.WriteLine(total * 4);
+            Console.ReadKey();
+        }
+    }
+}
