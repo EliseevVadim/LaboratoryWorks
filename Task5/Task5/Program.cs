fb7b4624102510cac6a9547beae487a6153c063a
EliseevVadim <eliseevv02@mail.ru>
2019-10-28 16:26:50 +0300

    Task5

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            bool b = false;
+            for (int x=1; x<N; x++)
+            {
+                for (int y=1; y<N; y++)
+                {
+                    for (int z=1; z<N; z++)
+                    {
+                        if (Math.Pow(x,3)+Math.Pow(y,3)+Math.Pow(z,3)==N)
+                        {
+                            Console.WriteLine($"{x}   {y}   {z}");
+                            b = true;
+                        }
+                        
+                    }
+                }
+            }
+            if (b==false)
+            {
+                Console.WriteLine("No such combinations!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
